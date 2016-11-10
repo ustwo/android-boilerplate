497e76193685a4109d06f898682df47577591acb
James Newman <numan1617@gmail.com>
2016-11-10 14:51:35 +0000

    Override application component with test application component

---
+++ app/src/androidTest/java/com/ustwo/boilerplate/application/TestApplicationModule.java
@@ -0,0 +1,22 @@
+package com.ustwo.boilerplate.application;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class TestApplicationModule {
+  private final Application application;
+
+  TestApplicationModule(@NonNull final Application application) {
+    this.application = application;
+  }
+
+  @Provides
+  Context provideContext() {
+    return application;
+  }
+}
