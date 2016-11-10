4c9e3c82e9565947ade12d224f9e62f04b758a19
James Newman <numan1617@gmail.com>
2016-11-10 16:27:16 +0000

    Add test for Hello world activity

---
+++ app/src/androidTest/java/com/ustwo/boilerplate/feature/helloworld/HelloWorldActivityTest.java
@@ -0,0 +1,28 @@
+package com.ustwo.boilerplate.feature.helloworld;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.ustwo.boilerplate.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+public class HelloWorldActivityTest {
+
+  @Rule
+  public ActivityTestRule<HelloWorldActivity> activityTestRule = new ActivityTestRule<>
+      (HelloWorldActivity.class, true, true);
+
+  @Test
+  public void helloWorld_textShown() {
+    onView(withId(R.id.textView_helloWorld))
+        .check(matches(allOf(withText(R.string.hello_world), isDisplayed())));
+  }
+}
