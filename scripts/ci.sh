b54e7cf6a08090f4dc2ffe7c86e65a14e4b68f0b
James Newman <numan1617@gmail.com>
2016-11-22 14:26:50 +0000

    Add comments on what the ci and remote testing scripts are doing

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -xe
 
+# Get the location for report directory from 1st param or use current directory
 if [ ! -z "$1" ]
 then
     report_location=$1
@@ -10,6 +11,8 @@ fi
 
 ./gradlew --no-daemon --info clean
 ./gradlew --no-daemon --info build countDebugDexMethods -PdisablePreDex
+
+# If we have a service key for the GCloud Cli then use remote testing otherwise run local tests
 if [ -n "${GCLOUD_SERVICE_KEY+1}" ]; then
     ./gradlew --no-daemon --info assembleAndroidTest -PdisablePreDex
     scripts/remoteTesting.sh $report_location
