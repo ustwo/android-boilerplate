b54bb5abe3dba556a0d5acb6e55356ef78696987
James Newman <numan1617@gmail.com>
2016-11-23 12:28:10 +0000

    Add leak canary

--- app/src/main/java/com/ustwo/boilerplate/application/BoilerplateApplication.java
+++ app/src/main/java/com/ustwo/boilerplate/application/BoilerplateApplication.java
@@ -1,10 +1,17 @@
 package com.ustwo.boilerplate.application;
 
 import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
 
 public class BoilerplateApplication extends Application {
 
   private final ApplicationComponent applicationComponent = createComponent();
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    initialiseLeakCanary();
+  }
 
   protected ApplicationComponent createComponent() {
     return DaggerApplicationComponent.builder().applicationModule(new ApplicationModule(this))
@@ -13,5 +20,14 @@ public class BoilerplateApplication extends Application {
 
   public ApplicationComponent getApplicationComponent() {
     return applicationComponent;
+  }
+
+  private void initialiseLeakCanary() {
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    LeakCanary.install(this);
   }
 }
