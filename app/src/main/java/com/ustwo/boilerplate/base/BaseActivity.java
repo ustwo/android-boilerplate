024c88f2160637acdc19cf6ec62753639bff0df8
James Newman <numan1617@gmail.com>
2016-11-30 11:51:17 +0000

    Add helper method to ease getting Application Component

--- app/src/main/java/com/ustwo/boilerplate/base/BaseActivity.java
+++ app/src/main/java/com/ustwo/boilerplate/base/BaseActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import com.ustwo.boilerplate.application.ApplicationComponent;
+import com.ustwo.boilerplate.application.BoilerplateApplication;
 
 public abstract class BaseActivity<V extends BaseView, C extends BaseComponent>
     extends AppCompatActivity {
@@ -21,6 +23,10 @@ public abstract class BaseActivity<V extends BaseView, C extends BaseComponent>
   protected void onDestroy() {
     getPresenter().onViewDetached();
     super.onDestroy();
+  }
+
+  protected ApplicationComponent getApplicationComponent() {
+    return ((BoilerplateApplication) getApplication()).getApplicationComponent();
   }
 
   @LayoutRes
